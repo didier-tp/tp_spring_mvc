3d971a9375f74719219d62da2f6d6feca4b3204b
DidierDefrance <didier@d-defrance.fr>
2024-06-11 10:29:09 +0200

    init

---
+++ tp/appliSpringMvc/src/main/java/tp/appliSpringMvc/AppliSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package tp.appliSpringMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliSpringMvcApplication.class, args);
+	}
+
+}
