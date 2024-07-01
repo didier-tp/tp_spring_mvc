d1c7458cb4a63841b6bf833f06a1d783f9ea989f
DidierDefrance <didier@d-defrance.fr>
2024-07-01 21:32:05 +0200

    s

--- tp/appliSpringMvc/src/main/java/tp/appliSpringMvc/rest/RestResponseEntityExceptionHandler.java
+++ tp/appliSpringMvc/src/main/java/tp/appliSpringMvc/rest/RestResponseEntityExceptionHandler.java
@@ -7,13 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import tp.appliSpringMvc.core.exception.MyNotFoundException;
 import tp.appliSpringMvc.dto.ApiError;
 
-@ControllerAdvice
+@ControllerAdvice(annotations = RestController.class)
 public class RestResponseEntityExceptionHandler 
    extends ResponseEntityExceptionHandler {
 	
--- tp/appliSpringMvc/src/main/java/tp/appliSpringMvc/web/controller/ErrorController.java
+++ tp/appliSpringMvc/src/main/java/tp/appliSpringMvc/web/controller/ErrorController.java
@@ -3,12 +3,13 @@ package tp.appliSpringMvc.web.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-//@ControllerAdvice
+@ControllerAdvice(annotations = Controller.class)
 public class ErrorController {
 
     private static Logger logger = LoggerFactory.getLogger(ErrorController.class);
